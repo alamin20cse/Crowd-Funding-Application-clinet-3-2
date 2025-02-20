7bac302da2269247379fbb5ed841b62c3e97bb18
alamin20cse <md.alamin165086@gmail.com>
2025-02-21 00:47:02 +0600

    firebbase initi

--- src/Shared/Footer.jsx
+++ src/Shared/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-      <footer className="footer bg-neutral text-gray-200 flex flex-col md:flex-row p-10 gap-10 lg:justify-between justify-center ">
+        <footer className="footer bg-neutral text-gray-200 flex flex-col md:flex-row p-10 gap-10 justify-between">
         <nav>
           <h6 className="footer-title">Campaign Management</h6>
           <a className="link link-hover">Fundraising Ideas</a>
@@ -9,7 +9,7 @@ const Footer = () => {
           <a className="link link-hover">Advertisement</a>
         </nav>
         <nav>
-          <h6 className="footer-title">Campaign</h6>
+          <h6 className="footer-titl e">Campaign</h6>
           <a className="link link-hover">About us</a>
           <a className="link link-hover">Contact</a>
         </nav>
